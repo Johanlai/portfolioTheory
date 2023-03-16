9475c8966245025438203a860aa87f56eb3b7e5b
johanlai <johanlai@hotmail.co.uk>
2023-03-16 00:20:57 +0000

    window test app

--- PortOptimiser.py
+++ PortOptimiser.py
@@ -10,28 +10,93 @@ import portfolioTheory as pt
 import yftickers as ticks
 import datetime as dt
 from dateutil.relativedelta import relativedelta
-
-def optimisePortfolio(tickers, start, end, optimiseBy= 'MaxSharpe', riskFreeRate=0, contraintSet=(0,1), logReturns = True, threshold=0.8, drop_extremes=True, excess=5, dateRange=None):
-    portfolio = pt.Portfolio(tickers, start, end)
-    portfolio.getData()
-    portfolio.cleanData(threshold=threshold, drop_extremes=drop_extremes, excess=excess, dateRange=dateRange)
-    portfolio.calculate_stats(logReturns=logReturns)
-    if logReturns:
-        meanReturns = portfolio.logReturns.mean()
-    else:
-        meanReturns = portfolio.returns.mean()
-    covMatrix = portfolio.covMatrix
-    if optimiseBy=='MaxSharpe':
-        def optimiser(meanReturns,covMatrix, riskFreeRate=riskFreeRate, contraintSet=contraintSet):
-            pt.maxSharpeRatio(meanReturns,covMatrix, riskFreeRate=riskFreeRate, contraintSet=contraintSet)
-    if optimiseBy=='MinVariance':
-        def optimiser(meanReturns, covMatrix, constraintSet=contraintSet):
-            pt.minimizeVariance(meanReturns, covMatrix, constraintSet=contraintSet)
+import matplotlib.pyplot as plt
+"""
+        def optimiser(self, meanReturns,covMatrix, riskFreeRate=riskFreeRate, contraintSet=contraintSet):
+            if optimiseBy=='MaxSharpe':
+                return pt.maxSharpeRatio(meanReturns,covMatrix, riskFreeRate=riskFreeRate, contraintSet=contraintSet)
+            if optimiseBy=='MinVariance':
+                return pt.minimizeVariance(meanReturns, covMatrix, constraintSet=contraintSet)
+            
+        if optimiseBy=='MaxSharpe':
+            def optimiser(self, meanReturns,covMatrix, riskFreeRate=riskFreeRate, contraintSet=contraintSet):
+                return pt.maxSharpeRatio(meanReturns,covMatrix, riskFreeRate=riskFreeRate, contraintSet=contraintSet)
+        if optimiseBy=='MinVariance':
+            def optimiser(self, meanReturns, covMatrix, riskFreeRate=riskFreeRate, constraintSet=contraintSet):
+                return pt.minimizeVariance(meanReturns, covMatrix, constraintSet=contraintSet)
+"""    
+class optPortfolio:
+    def __init__(self, tickers, start, end, optimiseBy= 'maxSharpe', riskFreeRate=0, contraintSet=(0,1), logReturns = True, threshold=0.8, drop_extremes=True, excess=5, dateRange=None):
+        self.portfolio = pt.Portfolio(tickers, start, end)
+        self.portfolio.getData()
+        self.portfolio.cleanData(threshold=threshold, drop_extremes=drop_extremes, excess=excess, dateRange=dateRange)
+        self.portfolio.calculate_stats(logReturns=logReturns)
+        self.start = start
+        self.end = end
+        self.optimiseBy = optimiseBy
+        self.riskFreeRate = riskFreeRate
+        self.constraintSet = contraintSet
+        if logReturns:
+            meanReturns = self.portfolio.logReturns.mean()
+        else:
+            meanReturns = self.portfolio.returns.mean()
+        covMatrix = self.portfolio.covMatrix
+        if (self.optimiseBy == 'maxSharpe'):
+            def optimiser(self):
+                return pt.maxSharpeRatio(meanReturns,covMatrix, riskFreeRate, contraintSet)
+        elif self.optimiseBy == 'minVol':
+            def optimiser(self):
+                return pt.minimizeVariance(meanReturns, covMatrix, contraintSet)
+        self.optimiser = optimiser(self)
         
+
+    def window_test(self):
+                # Forward Annual
+        shiftedRet = self.portfolio.logReturns.shift(-(len(self.portfolio.logReturns[self.portfolio.logReturns.index.year==self.portfolio.logReturns.index.year[0]])))
+        FannualReset = pd.DataFrame(np.full(len(self.portfolio.logReturns),fill_value=0.0),index=self.portfolio.logReturns.index)
+        FannualReset.rename(columns={0:'ret'}, inplace=True)
+        for i,x in self.portfolio.logReturns.groupby(self.portfolio.logReturns.index.year):
+            meanReturns = x.mean()
+            covMatrix = x.cov()
+            FannualReset['ret'][x.index]= shiftedRet.loc[x.index] @ (self.optimiser(meanReturns, covMatrix, riskFreeRate=self.riskFreeRate, constraintSet=self.contraintSet)[1][1]).T
+        FannualReset = FannualReset.shift(len(self.portfolio.logReturns[self.portfolio.logReturns.index.year==self.portfolio.logReturns.index[0].year])).dropna()
+        # Forward FBiAnnual
+        shiftedRetBannual = self.portfolio.logReturns.shift(-int(len(self.portfolio.logReturns[self.portfolio.logReturns.index.year==self.portfolio.logReturns.index.year[0]])/2))
+        FBiAnnualReset = pd.DataFrame(np.full(len(self.portfolio.logReturns),fill_value=0.0),index=self.portfolio.logReturns.index)
+        FBiAnnualReset.rename(columns={0:'ret'}, inplace=True)
+        for i,x in self.portfolio.logReturns.resample('6M',label='right',closed='left'):
+            meanReturns = x.mean()
+            covMatrix = x.cov()
+            FBiAnnualReset['ret'][x.index] = shiftedRetBannual.loc[x.index] @ (self.optimiser(meanReturns, covMatrix, riskFreeRate=self.riskFreeRate, constraintSet=self.contraintSet)[1][1]).T
+        FBiAnnualReset = FBiAnnualReset.shift(int(len(self.portfolio.logReturns[self.portfolio.logReturns.index.year==self.portfolio.logReturns.index.year[0]])/2))
+        # Forward Quarterly
+        shiftedRetQuarterly = self.portfolio.logReturns.shift(-int(len(self.portfolio.logReturns[self.portfolio.logReturns.index.year==self.portfolio.logReturns.index.year[0]])/2))
+        FQuarterlyReset = pd.DataFrame(np.full(len(self.portfolio.logReturns),fill_value=0.0),index=self.portfolio.logReturns.index)
+        FQuarterlyReset.rename(columns={0:'ret'}, inplace=True)
+        for i,x in self.portfolio.logReturns.resample('Q',label='right',closed='left'):
+            meanReturns = x.mean()
+            covMatrix = x.cov()
+            FQuarterlyReset['ret'][x.index] = shiftedRetQuarterly.loc[x.index] @ (self.optimiser(meanReturns, covMatrix, riskFreeRate=self.riskFreeRate, constraintSet=self.contraintSet)[1][1]).T
+        FQuarterlyReset = FQuarterlyReset.shift(int(len(self.portfolio.logReturns[self.portfolio.logReturns.index.year==self.portfolio.logReturns.index.year[0]])/4))
+        plt.figure(figsize=(10,4))
+        plt.title('Log returns')
+        plt.margins(x=0)
+        #plt.plot(np.cumsum(annualReset), label='Annual reset', ls=':')
+        plt.plot(np.cumsum(FannualReset.dropna()), label='FAnnual reset')
+        plt.plot(np.cumsum(FBiAnnualReset.dropna()), label='FBiAnnual reset')
+        plt.plot(np.cumsum(FQuarterlyReset.dropna()), label='FQuarterly reset')
+        plt.plot(np.cumsum(portfolio.portlogReturns), label='Equally weighted', ls='--')
+        plt.plot(np.cumsum(ftseLogReturn), label='ftse', ls='--')
+        plt.title('Out of sample - forward static window')
+        plt.grid()
+        plt.legend()
+        plt.show()                
 
 tickers = ticks.ftse100
 start=dt.datetime(2000,1,1)
 end=dt.datetime(2023,1,1)
 threshold=0.9
 
-port = optimisePortfolio(tickers, start, end, logReturns=True)
+port = optPortfolio(tickers, start, end, logReturns=True)
+port.window_test()
+
